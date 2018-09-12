e3dbcc6b7fa6342e04dc67a80705d41bbc2eca0f
Chribtoper <xcstolfax@gmail.com>
2018-09-12 21:36:40 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
 # your code goes here
+def begins_with_r(array)
+  arr = []
+array.each do |var|
+arr << var.start_with?('r')
+end
+if arr.include?(false)
+  false
+else
+  true
+end
+end
+
+def contain_a(array)
+  arr = []
+  array.collect do |var|
+    if var.include?('a') == true
+      arr << var
+    end
+  end
+    arr
+end
+
+def first_wa(array)
+  arr = array.collect do |var|
+    if var.to_s.include?("wa")
+      return var
+    end
+  end
+end
